6ad35502bbc5b6d2a15c07c0718ac42d6b7b9e43
Jerome Pratt <jerome.pratt@outlook.com>
2015-04-19 23:03:31 +0100

    starting the change to custom css

--- inc/header.php
+++ inc/header.php
@@ -12,26 +12,12 @@
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
-  <nav class="white" role="navigation">
-    <div class="nav-wrapper container">
       <a id="logo-container" href="#" class="brand-logo">Jerome Pratt</a>
-      <ul class="right hide-on-med-and-down">
-        <li class="portfolio <?php if ($section == "portfolio"){ echo "active"; }?>"><a href="portfolio.php">Portfolio</a></li>
-        <li class="about <?php if ($section == "about"){ echo "active"; }?>"><a href="about.php">About</a></li>
-        <li class="contact <?php if ($section == "contact"){ echo "active"; }?>"><a href="contact.php">Contact</a></li>
-      </ul>
-      <ul id="nav-mobile" class="side-nav">
+
         <li class="portfolio <?php if ($section == "portfolio"){ echo "active"; }?>"><a href="portfolio.php">Portfolio</a></li>
         <li class="about <?php if ($section == "about"){ echo "active"; }?>"><a href="about.php">About</a></li>
         <li class="contact <?php if ($section == "contact"){ echo "active"; }?>"><a href="contact.php">Contact</a></li>
 
-      </ul>
-
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
-    </div>
-  </nav>
 <div class="container">
 
